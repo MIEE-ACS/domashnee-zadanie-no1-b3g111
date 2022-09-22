40a85f04aa675e133448f08d493a4fe40dbdff4e
b3g111 <113125703+b3g111@users.noreply.github.com>
2022-09-22 21:03:44 +0300

    Add files via upload

---
+++ lab01/lab01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("r1=");
+            double r1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("r2=");
+            double r2 = Convert.ToDouble(Console.ReadLine());
+
+            if (r1 == 0 || r2 == 0 || r1 < 0 || r2 < 0)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            else
+            {
+                double s1 = 3.14 * Math.Pow(r1, 2);
+                Console.WriteLine("s1=" + s1);
+                double s2 = 3.14 * Math.Pow(r2, 2);
+                Console.WriteLine("s2=" + s2);
+                double s3 = s1 - s2;
+                Console.WriteLine("s3=" + s3);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
